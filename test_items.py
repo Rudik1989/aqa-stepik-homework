7fbe429b35c717bfcde3625299acec52bbfeb403
Rudik1989 <38017325+Rudik1989@users.noreply.github.com>
2021-04-02 00:49:35 +0300

    Update test_items.py
    update test_items.py

--- test_items.py
+++ test_items.py
@@ -2,8 +2,9 @@ import pytest
 from selenium import webdriver
 import time
 
+url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
 def test_add_to_basket_button_available(browser):
-    url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(url)
     time.sleep(30)
     button = len(browser.find_elements_by_class_name('btn-add-to-basket'))
